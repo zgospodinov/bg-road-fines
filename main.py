528e351644252610ce0f9513a1024642bfcac517
zdravko.gospodinov <zdravko.gospodinov@gmail.com>
2025-07-05 18:20:33 +0300

    Init app

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import smtplib
+from email.message import EmailMessage
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+
+
+
+def main():
+    print("Hello from bg-road-fines!")
+
+
+if __name__ == "__main__":
+    main()
