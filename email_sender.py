55b29b699c1984231a66464ffa484098744b4fcf
zdravko.gospodinov <zdravko.gospodinov@gmail.com>
2025-07-06 13:53:50 +0300

    Refactor email logic to class

--- email_sender.py
+++ email_sender.py
@@ -4,38 +4,33 @@ import ssl
 from dotenv import load_dotenv
 from email.message import EmailMessage
 
-
 load_dotenv()
 
-EMAIL_TO = os.getenv("EMAIL_TO")
-EMAIL_FROM = os.getenv("EMAIL_FROM")
-SMTP_SERVER = os.getenv("SMTP_SERVER")
-SMTP_PORT = int(os.getenv("SMTP_PORT"))
-SMTP_USER = os.getenv("SMTP_USER")
-SMTP_PASS = os.getenv("SMTP_PASS")
-E_USLUGI_MVR_URL = os.getenv("E_USLUGI_MVR")
+class EmailSender:
+    def __init__(self):
+        self.email_to = os.getenv("EMAIL_TO")
+        self.email_from = os.getenv("EMAIL_FROM")
+        self.smtp_server = os.getenv("SMTP_SERVER")
+        self.smtp_port = int(os.getenv("SMTP_PORT"))
+        self.smtp_user = os.getenv("SMTP_USER")
+        self.smtp_pass = os.getenv("SMTP_PASS")
+        self.e_uslugi_mvr_url = os.getenv("E_USLUGI_MVR")
 
+    def send_email(self, subject, body):
+        print("Sending email notification...")
+        content = f"""
+        {body}
+        <h2>За плащане, при наличие на глоби, посетете:</h2>
+        <a href="{self.e_uslugi_mvr_url}">Портал за електронни административни услуги на МВР</a>
+        """
+        msg = EmailMessage()
+        msg['Subject'] = subject
+        msg['From'] = self.email_from
+        msg['To'] = self.email_to
+        msg.add_alternative(content, subtype="html")
 
-def send_email(subject, body):
-    print("Sending email notification...")
-    
-    content = f"""
-    {body}
-
-    <h2>За плащане, при наличие на глоби, посетете:</h2>
-    <a href="{E_USLUGI_MVR_URL}">Портал за електронни административни услуги на МВР</a>
-    """
-
-    msg = EmailMessage()
-    msg['Subject'] = subject
-    msg['From'] = EMAIL_FROM
-    msg['To'] = EMAIL_TO
-    msg.add_alternative(content, subtype="html")
-
-    context = ssl.create_default_context()
-
-    with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT, context=context) as email_server:
-        email_server.login(SMTP_USER, SMTP_PASS)
-        email_server.send_message(msg)
-    
-    print("Email notification sent")
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(self.smtp_server, self.smtp_port, context=context) as email_server:
+            email_server.login(self.smtp_user, self.smtp_pass)
+            email_server.send_message(msg)
+        print("Email notification sent")
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-import email_sender
+from email_sender import EmailSender
 from road_fines import get_road_fines
 
 
-def main():
+def main():    
     print("Hello from bg-road-fines!")
+    email_service = EmailSender()
     road_fines_result = get_road_fines()
     
     # road_fines_result = None
     if(road_fines_result):
-        email_sender.send_email(
+        email_service.send_email(
         subject="Обобщена проверка на задължения по фиш, НП или споразумение",
         body=road_fines_result)
     else:
