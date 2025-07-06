744c12316ef1d7412d874817d2ad4b3eaecad36d
zdravko.gospodinov <zdravko.gospodinov@gmail.com>
2025-07-06 14:02:15 +0300

    Default error notification if external system is down

--- main.py
+++ main.py
@@ -4,16 +4,14 @@ from road_fines import get_road_fines
 
 def main():    
     print("Hello from bg-road-fines!")
+    
     email_service = EmailSender()
     road_fines_result = get_road_fines()
     
-    # road_fines_result = None
     if(road_fines_result):
-        email_service.send_email(
-        subject="Обобщена проверка на задължения по фиш, НП или споразумение",
-        body=road_fines_result)
+        email_service.send_email(body=road_fines_result)
     else:
-        pass
+        email_service.send_error_email(message=road_fines_result)
 
 if __name__ == "__main__":
     main()
